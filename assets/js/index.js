20554bcc74fb4f6e0fd134043bad6c331a1d6654
zraymene <33122649+zraymene@users.noreply.github.com>
2017-10-26 21:05:20 +0200

    Design Improvement.
    + Added Background changing animation .
    + Added 2 images for background .
    * Fixed Changing the size of web winodw ( Now website can resize to fix window on desktop + tablet window)

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,7 +16,37 @@ $(document).ready(function() {
 		
 	setInterval(function() {
 		$("div.text_hir").html(titles[Math.floor(Math.random()*titles.length)]);
-		console.log("y");
+		
 	},10000);
+	
+	
+	var imgs = Array("history_1.jpg",
+					"history.jpg",
+					"history_2.jpg");
+					
+	var img_c = 0;
+	var fade = true;
+	setInterval(function() {
+		if(fade){
+			fade = false;
+			$(".op_layer").animate({
+				  'background-color': 'rgba(120, 115, 115, 1)'
+				},2000,function() {
+					console.log(img_c + ""+ imgs.length);
+					if(img_c < imgs.length) {
+						$('html').css('background-image', 'url(' +'assets/img/'+ imgs[img_c] + ')');
+						$(".op_layer").animate({
+							'background-color': 'rgba(120, 115, 115, 0.54)'
+							},1000 , function() {fade = true;});
+						img_c += 1;
+					}else {
+						fade = true;
+						img_c = 0;
+					}
+				});
+			console.log("y");
+		}
+	},20000);
+		
 	
 });
