2f54806c2f74d59e7237632f61d5c32686a890d8
ImSilencer <dpgaming123234@gmail.com>
2017-10-28 02:16:59 +0700

    Updates 28/10/2017
    UPDATES:
    * Change the fade effect, loop time, transition color in index.js.
    * Fix the main layout size, position, navigation bar in style_sheet.css.
    *Add introductions to html.

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,10 +17,9 @@ $(document).ready(function() {
 	setInterval(function() {
 		$("div.text_hir").html(titles[Math.floor(Math.random()*titles.length)]);
 		
-	},10000);
+	},15000);
 	
-	
-	var imgs = Array("history_1.jpg",
+var imgs = Array("history_1.jpg",
 					"history.jpg",
 					"history_2.jpg");
 					
@@ -30,14 +29,14 @@ $(document).ready(function() {
 		if(fade){
 			fade = false;
 			$(".op_layer").animate({
-				  'background-color': 'rgba(120, 115, 115, 1)'
-				},2000,function() {
+				  'background-color': 'rgba(31, 31, 31, 1)'
+				},1500,function() {
 					console.log(img_c + ""+ imgs.length);
 					if(img_c < imgs.length) {
 						$('html').css('background-image', 'url(' +'assets/img/'+ imgs[img_c] + ')');
 						$(".op_layer").animate({
-							'background-color': 'rgba(120, 115, 115, 0.54)'
-							},1000 , function() {fade = true;});
+							'background-color': 'rgba(31, 31, 31, 0.54)'
+							},2000 , function() {fade = true;});
 						img_c += 1;
 					}else {
 						fade = true;
@@ -46,7 +45,7 @@ $(document).ready(function() {
 				});
 			console.log("y");
 		}
-	},20000);
-		
+	},10000);
+			
 	
 });
