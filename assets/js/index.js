05a175d21e682e41fe78315e2a3d83e7129e0b59
ImSilencer <dpgaming123234@gmail.com>
2017-10-28 19:10:29 +0700

    Small Update
    update notes

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,33 +19,33 @@ $(document).ready(function() {
 		
 	},15000);
 	
-var imgs = Array("history_1.jpg",
-					"history.jpg",
-					"history_2.jpg");
-					
-	var img_c = 0;
-	var fade = true;
-	setInterval(function() {
-		if(fade){
-			fade = false;
-			$(".op_layer").animate({
-				  'background-color': 'rgba(31, 31, 31, 1)'
-				},1500,function() {
-					console.log(img_c + ""+ imgs.length);
-					if(img_c < imgs.length) {
-						$('html').css('background-image', 'url(' +'assets/img/'+ imgs[img_c] + ')');
-						$(".op_layer").animate({
-							'background-color': 'rgba(31, 31, 31, 0.54)'
-							},2000 , function() {fade = true;});
-						img_c += 1;
-					}else {
-						fade = true;
-						img_c = 0;
-					}
-				});
-			console.log("y");
-		}
-	},10000);
-			
-	
-});
+//var imgs = Array("history_1.jpg",
+//					"history.jpg",
+//					"history_2.jpg");
+//					
+//	var img_c = 0;
+//	var fade = true;
+//	setInterval(function() {
+//		if(fade){
+//			fade = false;
+//			$(".op_layer").animate({
+//				  'background-color': 'rgba(31, 31, 31, 1)'
+//				},1500,function() {
+//					console.log(img_c + ""+ imgs.length);
+//					if(img_c < imgs.length) {
+//						$('html').css('background-image', 'url(' +'assets/img/'+ imgs[img_c] + ')');
+//						$(".op_layer").animate({
+//							'background-color': 'rgba(31, 31, 31, 0.54)'
+//							},2000 , function() {fade = true;});
+//						img_c += 1;
+//					}else {
+//						fade = true;
+//						img_c = 0;
+//					}
+//				});
+//			console.log("y");
+//		}
+//	},10000);
+//			
+//	
+//});  
