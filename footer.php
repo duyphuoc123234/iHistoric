60ac1a59dd17a1b9d4b50763dca4e82939e71fac
zraymene <33122649+zraymene@users.noreply.github.com>
2017-12-18 22:29:20 +0100

    Header & footer

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer class="footer-r">
+			<div class="footer-left">
+
+				<p class="footer-links" style="font-family: AFBA;">
+					<a href="#">Facebook</a>
+					·
+					<a href="#">Twiter</a>
+					·
+					<a href="#">About</a>
+					·
+					<a href="#">Faq</a>
+					·
+					<a href="#">Contact</a>
+				</p>
+				<p class="footer-company-name">iHistoric &copy; 2018</p>
+			</div>
+		
+</footer>
