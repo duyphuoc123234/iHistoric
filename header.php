c60ad3e0af7352ecbce016845a352807827c02db
ImSilencer <dpgaming123234@gmail.com>
2017-12-19 17:37:48 +0700

    push
    real push

--- header.php
+++ header.php
@@ -1,17 +1,17 @@
-<div class="bar" id="menu" >
+<div class="bar" id="bar-pos" >
 		<div class="logo">
 			<a href="#">iHISTORIC</a>
 			</div>
-			<ul>
-				<li><a href="#">Home</a></li>
-				<li><a href="#">Articles</a></li>
-				<li><a href="#">Support</a></li>
-				<li><?php
+			<div class="homo_link">
+				<a href="#">Home</a> 
+				<a href="#">Articles</a>
+				<a href="#">Support</a>
+				<?php
 					if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) { // Check if user logged in to change Log/sign in label with user first name
 						echo '<a href ="../iHistoric/login_system/profile.php">'.$_SESSION['first_name'].'<img id="dropdown" src="assets/img/dropdown.png" width="15px" height="15px"></a>';
 					}else {
 						echo '<a href ="../iHistoric/login_system/index.php">Log In or Sign Up</a>';
 					}
-					?></li>
-			</ul>
+				?>
+			</div>
 		</div>
